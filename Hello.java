175d33f1334e601596a9ab8aea2f0d58904b439c
williamsc45 <112980703+williamsc45@users.noreply.github.com>
2022-09-06 15:42:23 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,74 @@
+/***********************************************************************
+Lab 01
+
+Making ASCII Art
+Chris Williams
+8-25-2022
+CMSC 255
+***********************************************************************/
+
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("       LAB 01       ");
+		System.out.println("  Making ASCII Art  ");
+		System.out.println("   Chris Williams   ");
+		System.out.println("      8-25-2022     ");
+		System.out.println("      CMSC 255      ");	
+		System.out.println("^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("CCCCCCCCCCCCCCCCC");
+		System.out.println("C");
+		System.out.println("C");
+		System.out.println("C");
+		System.out.println("C");
+		System.out.println("C");
+		System.out.println("C");
+		System.out.println("C");
+		System.out.println("C");
+		System.out.println("CCCCCCCCCCCCCCCCC");
+		System.out.println();
+		System.out.println("H               H");
+		System.out.println("H               H");
+		System.out.println("H               H");
+		System.out.println("H               H");
+		System.out.println("HHHHHHHHHHHHHHHHH");
+		System.out.println("H               H");
+		System.out.println("H               H");
+		System.out.println("H               H");
+		System.out.println("H               H");
+		System.out.println("H               H");
+		System.out.println();
+		System.out.println("IIIIIIIIIIIIIIIII");
+		System.out.println("        I        ");
+		System.out.println("        I        ");
+		System.out.println("        I        ");
+		System.out.println("        I        ");
+		System.out.println("        I        ");
+		System.out.println("        I        ");
+		System.out.println("        I        ");
+		System.out.println("        I        ");
+		System.out.println("IIIIIIIIIIIIIIIII");
+		System.out.println();
+		System.out.println("RRRRRRRRRRRRRR");
+		System.out.println("R             R");
+		System.out.println("R             R");
+		System.out.println("R             R");
+		System.out.println("RRRRRRRRRRRRR");
+		System.out.println("R             R");
+		System.out.println("R             R");
+		System.out.println("R             R");
+		System.out.println("R             R");
+		System.out.println("R             R");
+		System.out.println();
+		System.out.println("    SSSSSSSSS    ");
+		System.out.println("   S         S   ");
+		System.out.println("   S         S   ");
+		System.out.println("     S           ");
+		System.out.println("       S         ");
+		System.out.println("         S       ");
+		System.out.println("           S     ");
+		System.out.println("   S         S   ");
+		System.out.println("   S         S   ");
+		System.out.println("    SSSSSSSSS    ");
+	}
+}
